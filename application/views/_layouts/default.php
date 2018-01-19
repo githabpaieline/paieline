62ee830c6c7b09d5d51771e18599abc020c299a5
sidibe <sidibe52@hotmail.com>
2018-01-19 20:16:56 +0000

    sa marche

--- application/views/_layouts/default.php
+++ application/views/_layouts/default.php
@@ -3,7 +3,9 @@
 <div class="container">
 	<div class="page-header"><h1><?php echo $page_title; ?></h1></div>
 	<section class="content">
+		<div align="center">
 		<?php $this->load->view($inner_view); ?>
+		</div>
 	</section>
 </div>
 
