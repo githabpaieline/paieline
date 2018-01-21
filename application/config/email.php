717f54574dc74d65e3f33f912a72622612a0f9fc
NDIAYE <modou.ndiaye@atos.net>
2018-01-21 14:51:00 +0100

    commit email config class

--- application/config/email.php
+++ application/config/email.php
@@ -8,12 +8,14 @@
 | */
 
 $config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.mailgun.org';
-$config['smtp_port'] = '587';
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';
+$config['smtp_port'] = '465';
 $config['smtp_timeout'] = '30';
-$config['smtp_user'] = 'postmaster@yourdomain.com';
-$config['smtp_pass'] = 'password';
-$config['charset'] = 'utf-8';
-$config['mailtype'] = 'html';
-$config['wordwrap'] = TRUE;
-$config['newline'] = "\r\n";
+$config['smtp_user'] = 'modou.pailine@gmail.com';
+$config['smtp_pass'] = 'm+a=reussite';
+//$config['mailtype'] = 'html';
+//$config['wordwrap'] = TRUE;
+
+$config['charset']='utf-8';  
+$config['newline']="\r\n";
+
